c57bd6678d85a166a804adc6c437fcb75615e8ce
First-name Family-name <115214769+Atsuki210@users.noreply.github.com>
2024-05-01 11:49:46 +0900

    コントローラー接続

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    public InputActionAsset inputActionAsset;
+    private InputAction moveAction;
+    private Vector2 moveInput;
+    void Awake(){
+        // InputActionから「PlayerControls」マップと「Move」アクションを取得
+        var actionMap = inputActionAsset.FindActionMap("PlayerControls");
+        moveAction = actionMap.FindAction("Move");
+
+        // アクションの有効化
+        moveAction.Enable();
+
+        // アクションにコールバックを設定
+        moveAction.performed += OnMovePerformed;
+        moveAction.canceled += OnMoveCanceled;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60; // ← FPS を 60 に設定
+    }
+
+    private void OnMovePerformed(InputAction.CallbackContext context){
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void OnMoveCanceled(InputAction.CallbackContext context){
+        moveInput = Vector2.zero;
+    }
+
+    void Update(){
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y) * speed * Time.deltaTime;
+        transform.Translate(move);
+    }
+}
